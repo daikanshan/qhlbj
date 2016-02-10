94259dd9a03af1720cf57569af305515a50ad4f5
daikanshan <daikanshan@163.com>
2016-02-10 08:51:31 +0800

    美化页面‘

--- app/models/admin/config.rb
+++ app/models/admin/config.rb
@@ -1,5 +1,15 @@
 class Admin::Config < ActiveRecord::Base
   mount_uploader :avatar, Admin::ConfigAvatarUploader
+  before_create :check_url
+  before_update :check_url
+
+  def check_url
+    if self.url =~ /(http:\/\/|ftp:\/\/|https:\/\/)/
+
+    else
+      self.url = "http://#{self.url}"
+    end
+  end
   def status
     if val ==1
       "显示"
